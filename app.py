7c7fc2449f68cc95315383a2a1e32f5b026943c5
alok <alokkumar991992@gmail.com>
2023-09-26 23:26:16 +0530

    made better

--- app.py
+++ app.py
@@ -1,24 +1,28 @@
 import torch
+import tensorflow as tf
 from diffusers import TextToVideoSDPipeline
 from diffusers import DiffusionPipeline, DPMSolverMultistepScheduler
 from diffusers.utils import export_to_video
 from PIL import Image
 import gradio as gr
 
-#pipe = DiffusionPipeline.from_pretrained("cerspense/zeroscope_v2_576w", torch_dtype=torch.float16)
+#loading diffusion model
 pipe1 = DiffusionPipeline.from_pretrained("damo-vilab/text-to-video-ms-1.7b", torch_dtype=torch.float16, variant="fp16")
 pipe1.scheduler = DPMSolverMultistepScheduler.from_config(pipe1.scheduler.config)
 
 pipe1.enable_model_cpu_offload()
-#pipe1.unet.enable_forward_chunking(chunk_size=1, dim=1)
 pipe1.enable_vae_slicing()
 
-def generate(description):
 
-  video_description = description
-  video_frames = pipe1(video_description, num_inference_steps=25).frames
-  video_path = export_to_video(video_frames)
-  return video_path
+def generate_video(description):
+  if description:
+    video_description = description
+    video_frames = pipe1(video_description, num_inference_steps=25).frames
+    video_path = export_to_video(video_frames)
+    return video_path
+  else:
+    gr.Warning("Please enter video description first")
+
 
 css='''
 #title_head{
@@ -35,7 +39,7 @@ with gr.Blocks(css=css) as front:
       gr.Markdown("<h1>Video Generation from Text</h1>", elem_id='title_head')
 
     with gr.Row():
-      description = gr.Text(
+      description = gr.Textbox(
       label='video description',
       show_label=False,
       max_lines=1,
@@ -44,10 +48,7 @@ with gr.Blocks(css=css) as front:
       btn = gr.Button('Generate video').style(full_width=False)
 
     with gr.Row():
-      result = gr.Video(label='Result', show_label=False, elem_id='video')
-    btn.click(generate,inputs=description,outputs=[result])
-
-
-
+      result = gr.Video(label='Result', show_label=False, elem_id='video', height=500, width=700)
+    btn.click(generate_video,inputs=description,outputs=[result])
 
 front.launch()
