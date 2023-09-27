3df1709148ce54b03dee8e4e80c9555199cdf8af
alok <alokkumar991992@gmail.com>
2023-09-27 12:41:16 +0530

    stopwords removal

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# video_generation_from_text
+# Video Generation from Text
+
+# Steps to run the Program:
+*   Go to **Runtime** in menu and select **Change runtime type**.
+*   Select **T4 GPU** and save it.
+*   Now Run each cell of the notebook (Shift+Enter). Wait for each cell to execute. 
+*   After running last cell, Gradio web interface will open.
+*   Enter video description in textbox and click Generate video button.
+*   Wait for few seconds till generated video is displayed.
+
+Note:  **T4 GPU**  must be selected before running. Program may crash if GPU resources get full.
--- app.py
+++ app.py
@@ -7,20 +7,29 @@ from PIL import Image
 import spacy
 import gradio as gr
 nlp=spacy.load('en_core_web_sm')
+import re
 
 #loading diffusion model
 pipe1 = DiffusionPipeline.from_pretrained("damo-vilab/text-to-video-ms-1.7b", torch_dtype=torch.float16, variant="fp16")
 
-
 pipe1.enable_model_cpu_offload()
 pipe1.enable_vae_slicing()
 
+def stopWords(str):
+  doc=nlp(str)
+  filtered_tokens = [token.text for token in doc if not token.is_stop]
+  description = ""
+  for word in filtered_tokens:
+    description += word + " "
+  return description.lstrip().rstrip()
 
 def generate_video(description):
+  description=re.sub('[^A-Za-z0-9 ]+','', description)
   pipe1.scheduler = DPMSolverMultistepScheduler.from_config(pipe1.scheduler.config)
+  
   if description:
-    video_description = description
-    # num_frames can be changed to conrol length of video. If set higher than 60, GPU runtime may fail.
+    video_description = stopWords(description)
+    # num_frames can be changed to control length of video. If set higher than 60, GPU runtime may fail.
     video_frames = pipe1(video_description, num_inference_steps=25, num_frames=30).frames
     video_path = export_to_video(video_frames)
     
